aed290c1604832566c00dc9f1b0d4f1c4ae9f567
Justin Black <justin.a.black@gmail.com>
2020-05-25 22:56:39 -0700

    Fixes tests in the test folder

--- samples/client/petstore/python-experimental/petstore_api/__init__.py
+++ samples/client/petstore/python-experimental/petstore_api/__init__.py
@@ -32,6 +32,7 @@ from petstore_api.configuration import Configuration
 
 # import exceptions
 from petstore_api.exceptions import OpenApiException
+from petstore_api.exceptions import ApiAttributeError
 from petstore_api.exceptions import ApiTypeError
 from petstore_api.exceptions import ApiValueError
 from petstore_api.exceptions import ApiKeyError
